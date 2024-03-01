ca4b1a3683fa2a1f9a5338a09f9572f1753112eb
Alex Burmitskiy <alexnoomad@gmail.com>
2024-03-01 15:31:58 +0600

    Очищение кода и устранение ошибок, добавил коментарии к функциям.

--- creatings_mobs.py
+++ creatings_mobs.py
@@ -1,8 +1,6 @@
-from functions import *
-
-
 class Boss:
     def __init__(self, name, damage, dmg_type, hp, weak, resist):
+        """Инициализация объекта Босс."""
         self.name = name
         self.damage = damage
         self.dmg_type = dmg_type
@@ -13,17 +11,19 @@ class Boss:
 
 class Hero:
     def __init__(self, name, damage, dmg_type, hp, resist, weak):
+        """Инициализация объекта Герой."""
         self.name = name
         self.damage = damage
         self.dmg_type = dmg_type
         self.hp = hp
         self.weak = weak
-        self.pick = 0
+        self.pick = 0  # Номер героя, выбранный игроком
         self.resist = resist
 
 
 class Mob:
     def __init__(self, name, damage, dmg_type, hp, weak, resist):
+        """Инициализация объекта Моб."""
         self.name = name
         self.damage = damage
         self.dmg_type = dmg_type
@@ -34,38 +34,37 @@ class Mob:
 
 # Создание боссов
 eikthyr = Boss("Eikthyr", 50, "Blunt", 1000, "Pierce", ["Slash"])
+# Новые идеи боссов
 '''the_elder = Boss("The Elder", [45, "Slash"], 1000, "Slash", ["Magic", "Pierce"])
 bonemass = Boss("Bonemass", [60, "Blunt"], 2000, "Blunt", ["Pierce"])
 modir = Boss("Modir", [70, "Frost"], 3000, "Blunt", ["Slash"])
 gullveig = Boss("Gullveig", [100, "Fire"], 4500, "Pierce", ["Blunt"])
 fenrir = Boss("Fenrir", [120, "Slash"], 10000, "Pierce", ["Blunt", "Frost"])
 jotunn = Boss("Jotunn", [200, "Blunt"], 20000, "Blunt", ["Slash", "Pierce"])'''
+# Добавьте других боссов после необходимости
 
 # Создание героев
 warrior = Hero("Warrior", 10, ["Slash"], 200, [], [""])
 archer = Hero("Archer", 20, ["Pierce"], 150, [], [""])
+# Новые идеи героев
 '''spartan = Hero("Spartan", [35, "Pierce"], 500, ["Slash"])
 mage = Hero("Wizard", [45, "Magic"], 350, ["Pierce"])
 mauler = Hero("Mauler", [25, "Blunt"], 800, ["Slash", "Pierce", "Blunt"])
 god_of_war = Hero("God of War", [10**100, "God"], 10**100, ["Slash", "Pierce", "Magic", "Blunt"])'''
-
+# Добавьте других героев после необходимости
 
 # Создание мобов
 spider = Mob("Spider", 5, ["Pierce"], 50, "Pierce", [])
-ice_golem = Mob("Ice golem", 20, ["Blunt"], 350, "Fire", ["Slash"])
+ice_golem = Mob("Ice Golem", 20, ["Blunt"], 350, "Fire", ["Slash"])
 dragon_of_inferno = Mob("Dragon Of Inferno", 40, ["Fire"], 650, "Frostbane", ["Slash"])
-'''skeleton = Mob("Skeleton", [10, "Slash"], 100, "Blunt", ["Slash", "Pierce"])
-draugr = Mob("Draugr", [25, "Slash"], 300, None, ["Magic"])
-wolf = Mob("Wolf", [35, "Pierce"], 150, "Slash", [None])
-troll = Mob("Troll", [40, "Blunt"], 650, "Pierce", ["Blunt"])'''
+# Добавьте других мобов после необходимости
 
-# Выбор по числам
-heroes = [warrior]
+# Выбор героев
+heroes = [warrior, archer]
 pick = 0
 for hero in heroes:
     pick += 1
-    hero.pick = pick
+    hero.pick = pick  # Присваиваем каждому герою уникальный номер
 
-# Рюкзачек героев
+# Рюкзак героев
 heroes_items = ["Heal Poison", "Damage Poison", "Strength Poison"]
-
--- functions.py
+++ functions.py
@@ -4,55 +4,45 @@ from creatings_mobs import *
 
 
 def have_hp():
+    """Проверяет наличие хитпоинтов у героев."""
     for hero in heroes:
         if hero.hp <= 0:
             heroes.remove(hero)
-    if not heroes:
-        return False
-    return True
+    return bool(heroes)
 
 
 def round_start(enemy):
-    print(f"Heroes left >>> |", end="")
+    """Описывает начало нового раунда битвы."""
+    print("Heroes left >>> |", end="")
     for hero in heroes:
         print(f"<{hero.name}>|", sep="", end="")
     print()
+
     damage = 0
     damage_type = ""
+
     while True:
-        curr_hero = input(f"Pick a Hero to attack > ")
+        curr_hero = input("Pick a Hero to attack > ")
         try:
             if int(curr_hero) > len(heroes):
                 print(f"Invalid answer, you have only {len(heroes)} heroes")
                 continue
-        except Exception as ex:
-            print(f"Invalid answer, try again.")
+        except ValueError:
+            print("Invalid answer, try again.")
             continue
+
         for hero in heroes:
             if curr_hero == str(hero.pick):
                 damage = hero.damage
                 damage_type = hero.dmg_type
                 curr_hero = hero
-        break
+                break
+
     time.sleep(0.5)
     print(f"{curr_hero.name} ready to fight, take a moment and ... ", end="")
-
     attack_res = attack()
-
-    curr_hero.hp -= attack_res[1]
-    if len(attack_res) != 3:
-        print(damage * attack_res[0])
-        if enemy.weak == damage_type:
-            print(f" x 2 (for {enemy.name} weakness)")
-        if enemy.resist == damage_type:
-            print(f" x 0.5 (for {curr_hero.name} weakness)")
-    if enemy.weak == damage_type:
-        damage *= 2
-    if enemy.resist == damage_type:
-        damage *= 0.5
-    enemy.hp -= damage * attack_res[0]
+    apply_attack_results(curr_hero, enemy, damage, damage_type, attack_res)
     print(f"Total Damage = {damage * attack_res[0]}")
-
     print()
     print(f"Enemy ({enemy.name}) HP Left = {enemy.hp} ")
     print(f"{curr_hero.name} HP Left = {curr_hero.hp}")
@@ -61,19 +51,14 @@ def round_start(enemy):
 
 
 def attack():
-    # Генерируем случайное время задержки
+    """Определяет результат атаки."""
     delay_time = random.uniform(3, 10)
-
     time.sleep(delay_time)
-
     start_time = time.time()
-    input("Punch!!!")
-    print()
-
-    # Считаем, сколько времени прошло от начала до момента ввода
+    input("Punch!!!\n\n")
     elapsed_time = time.time() - start_time
-
     print(f"You punch after {round(elapsed_time, 2)} seconds...")
+
     if elapsed_time < 0.2:
         print("It was too fast, you miss!!! And lose 30 hp!")
         return [0, 30, 0]
@@ -97,23 +82,49 @@ def attack():
         return [0, 30, 0]
 
 
-def enemy_atack(enemy):
+def apply_attack_results(curr_hero, enemy, damage, damage_type, attack_res):
+    """Применяет результаты атаки к героям и врагу."""
+    curr_hero.hp -= attack_res[1]
+
+    if len(attack_res) != 3:
+        print(damage * attack_res[0])
+
+        if enemy.weak == damage_type:
+            print(f" x 2 (for {enemy.name} weakness)")
+        if enemy.resist == damage_type:
+            print(f" x 0.5 (for {curr_hero.name} weakness)")
+
+    if enemy.weak == damage_type:
+        damage *= 2
+    if enemy.resist == damage_type:
+        damage *= 0.5
+
+    enemy.hp -= damage * attack_res[0]
+
+
+def enemy_attack(enemy):
+    """Описывает ход врага в битве."""
     print(f"{enemy.name} turn to Attack")
     print("Wait... ", end="")
     time.sleep(3)
     print("BAM!!!!")
     time.sleep(0.5)
+
     for hero in heroes:
-        damage = random.randrange(enemy.damage - int(enemy.damage * 0.2),
-                                  enemy.damage + int(enemy.damage * 0.2))
+        damage = random.randrange(
+            enemy.damage - int(enemy.damage * 0.2),
+            enemy.damage + int(enemy.damage * 0.2)
+        )
         hero.hp -= damage
-        print(hero.name, "took damage... ", f"-{damage}", f"hp. {hero.hp} hp Left")
+        print(f"{hero.name} took damage... ", f"-{damage}", f"hp. {hero.hp} hp Left")
         time.sleep(1)
+
     input("Continue >>> ")
     print()
 
 
 def check_person_stats():
+    """Выводит статистику персонажей."""
     print()
     while True:
         ch = input("Wanna check persons stats? [y/n] >>> ")
@@ -122,10 +133,10 @@ def check_person_stats():
                 heroe_weakness = [hero.weak for hero in heroes]
                 hero_dmg_type = [hero.dmg_type for hero in heroes]
                 hero_resists = [hero.resist for hero in heroes]
-                print(f"{hero.name}'s hp {hero.hp}"
-                      f" | damage {hero.damage} type{hero_dmg_type}"
-                      f" | weaknesses - {heroe_weakness}"
-                      f" | resists - {hero_resists}")
+                print(
+                    f"{hero.name}'s hp {hero.hp} | damage {hero.damage} type{hero_dmg_type} | "
+                    f"weaknesses - {heroe_weakness} | resists - {hero_resists}"
+                )
                 input("Continue >>> ")
                 print()
             break
@@ -136,21 +147,23 @@ def check_person_stats():
 
 
 def battle_with(enemy):
+    """Запускает битву с врагом."""
     print()
     check_person_stats()
-    while True:
-        if not have_hp():
-            print("All you heroes are dead...")
-            break
+
+    while have_hp():
         round_start(enemy)
         wanna_use_item(enemy)
+
         if enemy.hp <= 0:
             print(f"{enemy.name} defeated...")
             break
-        enemy_atack(enemy)
+
+        enemy_attack(enemy)
 
 
 def use_item(item, hero, enemy):
+    """Применяет предмет к герою или врагу."""
     if item == "Heal Poison":
         hero.hp += 200
     elif item == "Damage Poison":
@@ -160,8 +173,10 @@ def use_item(item, hero, enemy):
 
 
 def wanna_use_item(enemy):
+    """Проверяет, хочет ли игрок использовать предмет."""
     if not heroes_items:
-        return None
+        return
+
     while True:
         ch = input("Wanna use an item? [y/n] >>> ")
         if ch == "y":
@@ -169,6 +184,7 @@ def wanna_use_item(enemy):
             for item in heroes_items:
                 print(f"<{item}>|", end="")
             print()
+
             while True:
                 item = input("Which item do you wanna choose? >>> ")
                 if item not in heroes_items:
@@ -177,24 +193,27 @@ def wanna_use_item(enemy):
                     for hero in heroes:
                         print(f"<{hero.name}>|", sep="", end="")
                     print()
+
                     while True:
-                        ch_hero = input("On which hero? >>> ")
+                        choose_hero = input("On which hero? >>> ")
                         try:
-                            if int(ch_hero) > len(heroes):
+                            if int(choose_hero) > len(heroes):
                                 print(f"Invalid answer, you have only {len(heroes)} heroes")
                                 continue
-                        except Exception as ex:
-                            print(f"Invalid answer, try again.")
+                        except ValueError:
+                            print("Invalid answer, try again.")
                             continue
+
                         for hero in heroes:
-                            if ch_hero == str(hero.pick):
+                            if choose_hero == str(hero.pick):
                                 ch_hero = hero
-                        break
-                    use_item(item, ch_hero, enemy)
-                    heroes_items.remove(item)
-                    return None
+                                break
+
+                        use_item(item, ch_hero, enemy)
+                        heroes_items.remove(item)
+                        return
+
         elif ch == "n":
-            return None
+            return
         else:
             print("Invalid answer, try again.")
-
